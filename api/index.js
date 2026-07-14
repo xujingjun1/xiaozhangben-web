b0033fd3e8db690920639ddfc66b9ea7084f7ec8
xujingjun1 <xujingjun1@github.com>
2026-07-14 18:21:19 +0800

    fix: 后端使用环境变量端口

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ const DATA_DIR = path.join(__dirname, 'data')
 const DB_FILE = path.join(DATA_DIR, 'db.json')
 
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.use(cors())
 app.use(express.json({ limit: '10mb' }))
